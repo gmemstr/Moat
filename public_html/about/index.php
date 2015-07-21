668488ba68a64e465733828cf68ba047b142acde
Gabriel Simmer <bladesimmer@gmail.com>
2015-07-20 20:44:34 -0700

    Create index.php

---
+++ public_html/about/index.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="../style/styles.css"> 
+  <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300,400italic' rel='stylesheet' type='text/css'>
+  <title>Moat - Mobile Voat Browser</title>
+
+  </script>
+</head>
+
+<body>
+
+  <div id="feedback">
+    <a href="/moat/">HOME</a>
+  </div>
+
+  <h1>Moat - Mobile Voat About</h1>
+
+  <div id="faq">
+    <h2>WTF is Moat?</h2>
+    <p>Moat is a project by Gabriel Simmer to help bring 
+      <a href="http://voat.co">Voat</a> to a nicer mobile
+      interface.</p>
+      <br>
+
+      <h2>Why does it take so long to load sometimes?</h2>
+      <p>It's probably because the Voat cache has expired. Every
+        30 minutes it is refreshed, and during this time the site
+        can take a little while longer to load. Note that the caching
+        is done by Moat, it is not something Voat does :p</p>
+
+        <br>
+
+        <h2>What is it built with?</h2>
+        <p>Wizardy. But seriously, the site is running on
+          XAMPP for Windows, and using PHP to get and generate
+          the actual content from Voat. A plentiful of CSS is
+          used to make the site look nicer, and a small dabble
+          of jQuery from some fairly basic things (like the)
+          fade in when the page finally loads.</p>
+
+        </div>
+      </body>
+
+      </html>
