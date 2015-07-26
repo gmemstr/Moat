8c8da2ba2db6dcdf23fd602bedf752e051a8964a
Gabriel Simmer <bladesimmer@gmail.com>
2015-07-26 10:56:42 -0700

    Update index.php
    Added subverse switching :) Also temporarily disabled caching due to the nature of subverse switching.

--- public_html/index.php
+++ public_html/index.php
@@ -1,8 +1,13 @@
 <!doctype html>
 <html>
-
+<?php
+error_reporting(0);
+$sv = "all";
+if ($_GET['v'] != null) $sv = $_GET['v'];
+?>
 <head>
   <meta charset="UTF-8">
+   <meta name="theme-color" content="#33FF00">
   <link rel="stylesheet" href="style/styles.css"> 
   <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300,400italic' rel='stylesheet' type='text/css'>
   <title>Moat - Mobile Voat Browser</title>
@@ -14,16 +19,14 @@
     <a href="about/">ABOUT</a>
   </div>
 
-  <h1>Moat - Mobile Voat (/v/all placeholder)</h1>
+  <h1>Moat - /v/<?php echo $sv; ?></h1>
   <?php
   $cache = "chached.json"; //cache file
   $cacheTime = date("i", fileatime($cache)); //modified date of file
   $time = date("i");
 
-  error_reporting(0);
-
 //Refresh IF: time since last mod is more than 10 minutes, file doesn't exist, if nothing is in file
-  if (($cacheTime - $time) > 10 || !file_exists($cache) || file_get_contents($cache) == null){
+  //if ((($time - $cacheTime) / 5) % 5 || !file_exists($cache) || file_get_contents($cache) == null){
     $ch = curl_init();
 
 //cURL Options
@@ -36,17 +39,17 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); //<- TEMPORARY
 //curl_setopt($ch, CURLOPT_CAINFO, getcwd() . "\\cert\\voatcert.crt");
 
 //Last few options
-curl_setopt($ch, CURLOPT_URL,"https://fakevout.azurewebsites.net/api/v1/v/all"); //Placeholder sv
+curl_setopt($ch, CURLOPT_URL,"https://fakevout.azurewebsites.net/api/v1/v/" . $sv); //Placeholder sv
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-  'Content-Type: application/json', 'Your_Own_Key_Here' ));
+  'Content-Type: application/json', 'Voat-ApiKey: Your_Api_Key_Here' ));
 //--- 
 
 $res = curl_exec ($ch);
 file_put_contents($cache, $res);
 curl_close ($ch);
-}else{ //If file is up to date, found, and not empty
-  $res = file_get_contents($cache);
-}
+//}else{ //If file is up to date, found, and not empty
+// $res = file_get_contents($cache);
+//}
 
 $jdecode = json_decode($res, true); //decode the json into an array
 
@@ -68,7 +71,7 @@ for($i = 0; $i < sizeof($jdecode['data']); $i++){ //basic for loop
 */
 echo '<div id="post">';
 echo '<a href=post.php?sub=' . $jdecode['data'][$i]['subverse'] . "&id=" . $jdecode['data'][$i]['id'] . '>' . $jdecode['data'][$i]['title'] . '</a> ('
-  .$jdecode['data'][$i]['subverse'].')<br>';
+  . "<a href='?v=".$jdecode['data'][$i]['subverse']. "'>".$jdecode['data'][$i]['subverse'].'</a>)<br>';
 
 echo '<a href=https://fakevout.azurewebsites.net/user/' . $jdecode['data'][$i]['userName'] . 
 '>' . $jdecode['data'][$i]['userName'] . '</a> | ' . $jdecode['data'][$i]['upVotes'] . ' &#8593; '
